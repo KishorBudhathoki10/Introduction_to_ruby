23132bbc52da3ff1d388ea7122b2d29705d10947
Kishor Budhathoki <budhathokikishor10@gmail.com>
2018-11-16 12:27:53 +0100

    New files are created.

---
+++ the_basics/movies_dates.rb
@@ -0,0 +1,7 @@
+movies_with_date = {"Fire" => 1975, "Water" => 2004, "Land" => 2013, "Air" => 2001, "Sky" => 1981}
+
+puts movies_with_date["Fire"]
+puts movies_with_date["Water"]
+puts movies_with_date["Land"]
+puts movies_with_date["Air"]
+puts movies_with_date["Sky"]
